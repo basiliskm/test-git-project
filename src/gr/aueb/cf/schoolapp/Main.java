4884068613d571c935be11ecfde62580d85ec79e
basiliskm <bi.karami@gmail.com>
2026-05-18 18:49:39 +0300

    feat: add hello

--- src/gr/aueb/cf/schoolapp/Main.java
+++ src/gr/aueb/cf/schoolapp/Main.java
@@ -3,5 +3,6 @@ package gr.aueb.cf.schoolapp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Coding Factory");
+        System.out.println("Hello 2");
     }
 }
